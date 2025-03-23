e8342c69a452573beac52ff1c6726617612ba9de
tat <tatsu0823takasago@icloud.com>
2025-03-23 14:06:19 +0900

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 // import netlify from '@astrojs/netlify/functions'; // 使わないのでコメントアウト
 
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone' // または 'middleware'
-  })
+  adapter: netlify()
 });
